0602bd21cbbe6b7e6d95e2d2d45ee71ccb4692c2
Michael Easter <codetojoy@gmail.com>
2024-05-18 10:53:39 -0300

    script checks

--- egg_minimal_runner_v15/ebean-runner/run_simple.sh
+++ egg_minimal_runner_v15/ebean-runner/run_simple.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+set -e
+
+echo "checking for generated ebean.properties ..."
+stat src/main/resources/ebean.properties > /dev/null 2>&1 
+
 mvn spring-boot:run -Dstart-class=net.codetojoy.ebean.app.App 
 
