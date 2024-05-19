9398a7a565268c9e01dbac3c2f7ac6113ed1f931
Michael Easter <codetojoy@gmail.com>
2024-05-19 05:53:26 -0300

    rename ebean.properties to application.properties

--- egg_commandline_min_v13/ebean-runner/run_full.sh
+++ egg_commandline_min_v13/ebean-runner/run_full.sh
@@ -2,8 +2,8 @@
 
 set -e 
 
-echo "checking for generated ebean.properties ..."
-stat src/main/resources/ebean.properties > /dev/null 2>&1 
+echo "checking for generated application.properties ..."
+stat src/main/resources/application.properties > /dev/null 2>&1 
 
 mvn clean compile spring-boot:run -Dstart-class=net.codetojoy.ebean.app.App 
 
