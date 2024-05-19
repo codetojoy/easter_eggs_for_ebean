48a7e84aa9a9243287d53b56f76994b0758b13da
Michael Easter <codetojoy@gmail.com>
2024-05-19 05:34:11 -0300

    clean up App

--- egg_minimal_runner_v13/ebean-runner/src/main/java/net/codetojoy/ebean/app/App.java
+++ egg_minimal_runner_v13/ebean-runner/src/main/java/net/codetojoy/ebean/app/App.java
@@ -2,84 +2,38 @@ package net.codetojoy.ebean.app;
 
 import java.util.*;
 
-import net.codetojoy.ebean.model.Address;
+import net.codetojoy.ebean.model.*;
 import net.codetojoy.ebean.model.Customer;
 
 import io.ebean.DB;
 import io.ebean.Database;
 import io.ebean.annotation.Transactional;
 
-public class App {
+// for original, see https://github.com/eugenp/tutorials/tree/master/libraries-data-db/src/main/java/com/baeldung/libraries/ebean/app
 
-    public static void main(String[] args) {
-        // insertAndDeleteInsideTransaction();
-        // crudOperations();
+public class App {
+    
+    void run() {
         queryCustomers();
     }
 
-/*
-    @Transactional
-    public static void insertAndDeleteInsideTransaction() {
-
-        Customer c1 = getCustomer();
-        Database server = DB.getDefault();
-        server.save(c1);
-        Customer foundC1 = server.find(Customer.class, c1.getId());
-        server.delete(foundC1);
+    public static void main(String[] args) {
+        try {
+            App app = new App();
+            app.run();
+        } catch (Exception ex) {
+            System.err.println("caught exception: " + ex.getMessage());
+        }
     }
 
-    public static void crudOperations() {
-    	
-        Address a1 = new Address("5, Wide Street", null, "New York");
-        Customer c1 = new Customer("John Wide", a1);
-
-        Database server = DB.getDefault();
-        server.save(c1);
-
-        c1.setName("Jane Wide");
-        c1.setAddress(null);
-        server.save(c1);
-
-        Customer foundC1 = DB.find(Customer.class, c1.getId());
-
-        DB.delete(foundC1);
-    }
-*/
-
-    public static void queryCustomers() {
-/*
-        Address a1 = new Address("1, Big Street", null, "New York");
-        Customer c1 = new Customer("Big John", a1);
-
-        Address a2 = new Address("2, Big Street", null, "New York");
-        Customer c2 = new Customer("Big John", a2);
-
-        Address a3 = new Address("3, Big Street", null, "San Jose");
-        Customer c3 = new Customer("Big Bob", a3);
-
-        DB.saveAll(Arrays.asList(c1, c2, c3));
-*/
-
+    void queryCustomers() {
         List<Customer> customers = DB.find(Customer.class)
-            .select("name, id, email")
-            // .fetch("id", "email")
-            // .where()
-            // .eq("city", "San Jose")
-            .findList();
+                                     .select("name, id, email")
+                                     .findList();
 
         for (Customer customer : customers) {
             System.out.println("TRACER VICTORY: " + customer.toString());
     
         }
-
-//         DB.deleteAll(Arrays.asList(c1, c2, c3));
     }
-
-    private static Customer getCustomer() {
-        // Address a1 = new Address("1, Big Street", null, "New York");
-        // Customer c1 = new Customer("Big John", a1);
-        // return c1;
-        return null;
-    }
-
 }
